28c893b8a3fd9caa5e762b77eaf0966329319686
wendycwong <wendycwong@yahoo.com>
2017-04-26 15:53:44 -0700

    Added pyunit test that will create a hanging in ForJoinWorkerThread.java.

---
+++ h2o-py/tests/testdir_algos/pca/pyunit_pubdev_3500_max_k_randomized.py
@@ -0,0 +1,30 @@
+from __future__ import print_function
+from builtins import range
+import sys
+
+sys.path.insert(1, "../../../")
+import h2o
+from tests import pyunit_utils
+from h2o.transforms.decomposition import H2OPCA
+
+
+def pca_max_k():
+
+    data = h2o.upload_file(pyunit_utils.locate("smalldata/prostate/prostate_cat.csv"))
+    x = list(set(data.names))
+    pcaRandomized = H2OPCA(k=-1, transform="STANDARDIZE", pca_method="GramSVD", use_all_factor_levels=True,
+                           impute_missing=True, max_iterations=100, seed=12345)
+    pcaRandomized.train(x, training_frame=data)
+    pcaRandomized = H2OPCA(k=-1, transform="STANDARDIZE", pca_method="Randomized", use_all_factor_levels=True,
+                           impute_missing=True, max_iterations=100, seed=12345)
+    pcaRandomized.train(x, training_frame=data)
+
+    pcaRandomized = H2OPCA(k=-1, transform="STANDARDIZE", pca_method="Randomized", use_all_factor_levels=True,
+                           impute_missing=True, max_iterations=100, seed=12345)
+    pcaRandomized.train(x, training_frame=data)
+
+
+if __name__ == "__main__":
+    pyunit_utils.standalone_test(pca_max_k)
+else:
+    pca_max_k()
